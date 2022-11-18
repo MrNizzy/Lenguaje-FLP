2af9773a2dc8df09638cf80a4d630a1a7c1d9045
Nizzy Hernandez <3134183631y@gmail.com>
2022-11-17 20:37:11 -0500

    Update: Implementación gramática
    Se implementó las nuevas caracteristicas de la gramática, incluyendo solución de errores menores

--- main.rkt
+++ main.rkt
@@ -6,14 +6,15 @@
     (espacio-blanco (whitespace) skip)
     (comentario ("#" (arbno (not #\newline))) skip)
     (identificador (letter (arbno (or letter digit "?" "$"))) symbol)
+    (texto ("\"" (arbno (or letter digit "?" "$" "-" "#" "." whitespace)) "\"") string)
     (numero (digit (arbno digit)) number)
     (numero ("-" digit (arbno digit)) number)
     (numero (digit (arbno digit) "." digit (arbno digit)) number)
     (numero ("-" digit (arbno digit) "." digit (arbno digit)) number)
     (numero-hexa ("0x" (or digit "a" "b" "c" "d" "e" "f")
-                       (arbno (or digit "a" "b" "c" "d" "e" "f"))) symbol)
+                       (arbno (or digit "a" "b" "c" "d" "e" "f"))) string)
     (numero-octal ("0o" (or "0" "1" "2" "3" "4" "5" "6" "7")
-                        (arbno (or "0" "1" "2" "3" "4" "5" "6" "7"))) symbol)
+                        (arbno (or "0" "1" "2" "3" "4" "5" "6" "7"))) string)
     )
   )
 
@@ -23,10 +24,11 @@
     ;-------------------------------------------EXPRESIONES-------------------------------------------
     (programa (expresion) a-program)
     (expresion (numero) num-exp)
-    (expresion (numero-hexa) numhexa-exp)
-    (expresion (numero-octal) numoctal-exp)
+    (expresion (numero-hexa) numHexa-exp)
+    (expresion (numero-octal) numOctal-exp)
     (expresion (identificador) var-exp)
-
+    (expresion (texto) texto-exp)
+    
     ;......................................EXPRESIONES BOOLEANAS......................................
     (expresion ("true") true-exp)
     (expresion ("false") false-exp)
@@ -35,22 +37,32 @@
     (expresion ("if" expresion ":" expresion "else" expresion) if-exp)
 
     ;........................................EXPRESION INFIJA.........................................
-    (expresion ("math" "(" expresion (arbno primitiva expresion) ")") prim-exp)
+    (expresion ("(" expresion (arbno primitiva expresion) ")") prim-exp)
 
     ;.......................................EXPRESIONES LOCALES.......................................
     (expresion ("let" (arbno identificador "=" expresion) "in" expresion) let-exp)
 
     ;.........................................PROCEDIMIENTOS..........................................
     (expresion ("proc" "(" (separated-list identificador ",") ")" expresion) proc-exp)
-    (expresion ("(" expresion (arbno expresion) ")") app-exp)
+    (expresion ("[" expresion (arbno expresion) "]") app-exp)
 
     ;...........................................ASIGNACION............................................
     (expresion ("modify" expresion (arbno ";" expresion) "end") modify-exp)
     (expresion ("set" identificador "=" expresion) set-exp)
+    
+    ;.............................................UNARIAS.............................................
+    (expresion (primitiva-unaria "(" expresion ")") unaria-exp)
+    (expresion (primitiva-unariaStrings "(" expresion (arbno "+" expresion) ")") unariaStrings-exp)
 
     ;...........................................WHILE & FOR...........................................
     (expresion ("while" expresion ":" expresion) while-exp)
     (expresion ("for" expresion "in" expresion ":" expresion) for-exp)
+
+    ;...........................................ESTRUCTURA............................................
+    (expresion ("struct" identificador "=" "{" identificador ":" expresion
+                         (arbno "," identificador ":" expresion) "}") struct-exp)
+    (expresion ("get" identificador "." identificador) getStruct-exp)
+    (expresion ("send" identificador "." identificador "=" expresion) sendStruct-exp)
 
     ;--------------------------------------------PRIMITIVAS-------------------------------------------
     
@@ -60,21 +72,22 @@
     (primitiva ("*") mult-prim)
     (primitiva ("%") mod-prim)
     (primitiva ("/") div-prim)
-    (primitiva ("add1") add-prim)
-    (primitiva ("sub1") sub-prim)
     ;......................................PRIMITIVAS BOOLEANAS.......................................
     (primitiva ("<") menor-prim)
     (primitiva (">") mayor-prim)
-    (primitiva ("<=") menorigual-prim)
-    (primitiva (">=") mayorigual-prim)
+    (primitiva ("<=") menorIgual-prim)
+    (primitiva (">=") mayorIgual-prim)
     (primitiva ("==") igual-prim)
     (primitiva ("!=") diferente-prim)
     (primitiva ("and") and-prim)
     (primitiva ("or") or-prim)
     (primitiva ("not") not-prim)
+    ;.......................................PRIMITIVAS UNARIAS........................................
+    (primitiva-unaria ("add1") add-prim)
+    (primitiva-unaria ("sub1") sub-prim)
     ;......................................PRIMITIVAS DE CADENAS......................................
-    (primitiva ("length") length-prim)
-    (primitiva ("concat") concat-prim)
+    (primitiva-unaria ("length") length-prim)
+    (primitiva-unariaStrings ("concat") concat-prim)
     )
   )
 
